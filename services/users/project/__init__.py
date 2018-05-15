f0b883de70911effbc23eceef911809441556177
Hugo Levasseur <hugo@Hugos-MacBook-Pro.local>
2018-05-15 16:05:43 -0400

    small fixes (where flask runs)

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,6 @@
 # services/users/project/__init__.py
 
+import os
 from flask import Flask, jsonify
 
 # instantiate the app
@@ -7,7 +8,11 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object ('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object('project.config.DevelopmentConfig')
+
+import sys
+print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
