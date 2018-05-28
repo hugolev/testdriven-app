44d964c4d527d3e24e121c0e31bd3d49fc26993e
Hugo Levasseur <hugo@Hugos-MacBook-Pro.local>
2018-05-27 23:24:09 -0400

    latest update

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,9 +2,63 @@
 
 from flask.cli import FlaskGroup
 
-from project import app
+from project import create_app, db
 
-cli = FlaskGroup(app)
+from project.api.models import User
+
+import unittest
+
+import coverage
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
+COV = coverage.coverage(
+        branch=True,
+        include='project/*',
+        omit=[
+            'project/tests/*',
+            'project/config.py',
+            ]
+        )
+COV.start()
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Sumary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command()
+def test():
+    """ Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+@cli.command()
+def seed_db():
+    """ Seeds the database """
+    db.session.add(User(username='hugo', email="hugo@le.com"))
+    db.session.add(User(username='herman', email="herman@le.com"))
+    db.session.commit()
 
 if __name__ == '__main__':
         cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,23 +1,30 @@
 # services/users/project/__init__.py
 
 import os
-from flask import Flask, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the app
+# instantiate db
+db = SQLAlchemy()
 
-app = Flask(__name__)
 
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object('project.config.DevelopmentConfig')
+def create_app(script_info=None):
 
-import sys
-print(app.config, file=sys.stderr)
+    # instantiate the app
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-        })
+    app = Flask(__name__)
 
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # setup extensions
+    db.init_app(app)
+
+    # register  blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    app.shell_context_processor({'app': app, 'db': db})
+    return app
